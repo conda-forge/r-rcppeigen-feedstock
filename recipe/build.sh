eeccd586d5edc05212dce5322d9d9c3550613712
Christian Brueffer <christian@brueffer.io>
2021-12-13 13:27:37 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,8 @@
 
 export DISABLE_AUTOBREW=1
 
+# workarond conda-forge/r-base-feedstock#163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
